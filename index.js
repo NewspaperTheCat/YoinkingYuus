f5258217cf9ebdd190dbe7e4a1f98c6a13f0fec5
NewspaperTheCat <jmprendergast@wpi.edu>
2026-04-17 15:48:55 -0400

    Brought in very simple webgl set up

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Happy developing ✨')
+//=====================================================
+// Yoinking Yuus
+// James Prendergast, Zack Robinson, Trey Bowen
+//=====================================================
+
+
+let gl;
+let program;
+let canvas;
+
+window.onload = function init() {
+    canvas = document.getElementById("gl-canvas");
+    gl = WebGLUtils.setupWebGL(canvas, null);
+    if (!gl) { alert("WebGL isn't available"); }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.8, 0.9, 1.0, 1.0);
+
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    render();
+};
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    requestAnimationFrame(render);
+}
